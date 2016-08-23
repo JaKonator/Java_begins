812cf895db6c1e610bcedf2122aab33f91b7c24e
JaKonator <stephan-alois.jachs@stud.unileoben.ac.at>
2016-08-23 09:39:36 +0200

    Added DerBeginn_1
    Added the application DerBeginn_1.java

---
+++ DerBeginn_1.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class DerBeginn_1{
+
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "\"So\nbeginnt\nbeinahe\njeder\nProgrammierkurs");
+  }
+}
