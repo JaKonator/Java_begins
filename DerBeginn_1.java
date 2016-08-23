d1d82f606405ec717f5f12e60ff5452d97725403
JaKonator <stephan-alois.jachs@stud.unileoben.ac.at>
2016-08-23 09:45:59 +0200

    Text changed
    Changed from German to English

--- DerBeginn_1.java
+++ DerBeginn_1.java
@@ -1,8 +1,14 @@
+/** This is how every course in programming is starting
+*
+**/
+
 import javax.swing.JOptionPane;
 
 public class DerBeginn_1{
 
   public static void main(String[] args) {
-    JOptionPane.showMessageDialog(null, "\"So\nbeginnt\nbeinahe\njeder\nProgrammierkurs");
+
+    JOptionPane.showMessageDialog(null, "\"So\nstarts\nevery\ncourse\n
+      in\nprogramming");
   }
 }
